5df555681f433eb3103c33f9b1bf7dec52387c7b
Nahuel Fernandez Beschtedt <lnahuelfernandezb@gmail.com>
2022-04-09 20:15:39 -0300

    Separacion de back y front

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors')
+
 const app = express();
 const logger = require('./loggerMiddlewere');
 
+app.use(cors())
 app.use(express.json());
 
 app.use(logger);
